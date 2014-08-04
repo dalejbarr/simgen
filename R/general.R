dab9476798761525541151c68060fa08b925f3ea
dalejbarr <dalejbarr3@gmail.com>
2014-08-04 12:07:03 +0100

    a few small fixes

--- R/general.R
+++ R/general.R
@@ -1,7 +1,6 @@
 #' Fit model with error-trapping
 #' 
-#' Try to fit an lmer model, catching any errors/warnings
-#' 
+#' Try to fit an lmer model, catching any warnings about convergence
 #' 
 #' @param tf.formula The model formula
 #' @param tf.data The data frame
@@ -9,7 +8,7 @@
 #' @return A list with:
 #' \item{value}{fitted model object from lmer}
 #' \item{converged}{whether or not the model converged}
-#' @seealso \code{\link{fitlmer}}
+#' @seealso \code{\link{tryUpdate}}
 #' @examples
 #' 
 #' # random intercepts only
@@ -28,6 +27,42 @@ tryFit <- function(tf.formula, tf.data, ...) {
     arg.list <- c(list(formula=tf.formula, data=tf.data), list(...))
     list(value=withCallingHandlers(tryCatch(
              do.call(lme4:::lmer, arg.list),
+             error=function(e) e),
+             warning=w.handler),
+         converged=converged)
+}
+
+
+#' Update model with error-trapping
+#' 
+#' Try to update an lmer model, catching any errors/warnings
+#' 
+#' @param tf.formula The model formula
+#' @param tf.model The data frame
+#' @param ... Further arguments to be passed onto the function \code{\link{update}}
+#' @return A list with:
+#' \item{value}{fitted model object from lmer}
+#' \item{converged}{whether or not the model converged}
+#' @seealso \code{\link{tryFit}}
+#' @examples
+#' mod1 <- tryFit(Resp~Cond+(1+Cond|SubjID)+(1+Cond|ItemID), mkDf())
+#' mod2 <- tryUpdate(.~.-Cond, mod1)
+#' lrTest(mod1, mod2)
+#'
+#' @importFrom lme4 lmer
+#' @importClassesFrom lme4 mer
+#' @importMethodsFrom lme4 summary show update
+#' @export tryUpdate
+tryUpdate <- function(tu.formula, tu.model, ...) {
+    converged <- TRUE
+    w.handler <- function(w) {
+        converged <- FALSE
+        invokeRestart("muffleWarning")
+    }
+    m1 <- tu.model$value
+    arg.list <- c(list(object=m1, formula=tu.formula), list(...))
+    list(value=withCallingHandlers(tryCatch(
+             do.call(update, arg.list),
              error=function(e) e),
              warning=w.handler),
          converged=converged)
@@ -153,30 +188,8 @@ contr.deviation <- function(n) {
 }
 
 
-
-#' Get p-value from model comparison of lmer objects
-#'
-#' Derive p-value by comparing two models fitted by
-#' \code{\link{tryFit}}.
-#'
-#' @param m1 A model fitted by \code{\link{tryFit}}
-#' @param m2 A model fitted by \code{\link{tryFit}}
-#' @return A vector with:
-#' \item{chisq}{observed chi-square (difference in -2LL for models)}
-#' \item{df}{degrees of freedom for the comparison}
-#' \item{p}{p-value}
-#' @examples
-#' d1 <- mkDf()
-#' 
-#' m1 <- tryFit(Resp ~ Cond + (1 + Cond | SubjID) + (1 + Cond | ItemID), d1, REML=FALSE,
-#'               na.action=na.omit)
-#' m2 <- tryFit(Resp ~ (1 + Cond | SubjID) + (1 + Cond | ItemID), d1, REML=FALSE,
-#'               na.action=na.omit)
-#'
-#' getLmer.pValue(m1, m2)
-#' 
+# NOT EXPORTED!
 #' @importFrom lme4 lmer
-#' @export getLmer.pValue
 getLmer.pValue <- function(m1,m2) {
     pval <- c(chisq=NA, df=NA, p=NA)
     if (m1$converged && m2$converged) {
@@ -189,7 +202,50 @@ getLmer.pValue <- function(m1,m2) {
     return(pval)
 }
 
-getLmer.chisq <- function(m1,m2) {
+#' Likelihood-Ratio Test for Fitted mer Objects
+#'
+#' @param m1 Model to be compared (list object from \code{\link{tryFit}})
+#' @param m2 Model to be compared (list object from \code{\link{tryFit}})
+#'
+#' @return A list with:
+#' \item{Number of Estimated Parameters}{A matrix;}
+#' \item{Likelihood-Ratio Test}{Output from the test, or \code{NA} if either or both models did not converge.}
+#' @seealso \code{\link{tryFit}}
+#' @examples
+#' dat <- mkDf()
+#' m1 <- tryFit(Resp~Cond+(1+Cond|SubjID)+(1+Cond|ItemID), dat, REML=FALSE)
+#' m2 <- tryFit(Resp~(1+Cond|SubjID)+(1+Cond|ItemID), dat, REML=FALSE)
+#' lrTest(m1, m2)
+#' @export lrTest
+lrTest <- function(m1, m2) {
+    m1v <- m1$value; m2v <- m2$value
+    # calculate no. estimated parameters
+    m1d <- m1v@dims
+    m2d <- m2v@dims
+    m1.df <- c(rand=m1d["nt"], fixed=m1d["p"], link=m1d["s"])
+    m1.df <- c(m1.df, tot=sum(m1.df))
+    m2.df <- c(rand=m2d["nt"], fixed=m2d["p"], link=m2d["s"])
+    m2.df <- c(m2.df, tot=sum(m2.df))
+    df.mx <- rbind(m1=m1.df, m2=m2.df)
+    colnames(df.mx) <- c("random","fixed","link","total")
+    # get estimated log likelihood (deviance)
+    if (m1$converged && m2$converged) {
+        dv.m1 <- c(m1=-2*lme4::logLik(m1v),
+                   m2=-2*lme4::logLik(m2v))
+        dv.m1 <- c(dv.m1, chisq=as.numeric(abs(dv.m1["m2"]-dv.m1["m1"])))
+        diff.df <- abs(df.mx["m2","total"]-df.mx["m1","total"])
+        dv.m1 <- c(dv.m1, df=diff.df)
+        dv.m1 <- c(dv.m1, p=as.numeric(pchisq(abs(dv.m1["chisq"]), diff.df, lower.tail=FALSE)))
+        res <- data.frame(deviance1=round(dv.m1["m1"],3), deviance2=round(dv.m1["m2"],3),
+                          chisq=round(dv.m1["chisq"],3), df=as.integer(dv.m1["df"]), p=dv.m1["p"])
+    } else {
+        res <- data.frame(deviance1=NA, deviance2=NA, chisq=NA, df=NA, p=NA)
+    }
+    list(`Number of Estimated Parameters`=df.mx,
+         `Likelihood-Ratio Test`=res)
+}
+
+.getLmer.chisq <- function(m1,m2) {
     pval <- NA
     if (m1$converged && m2$converged) {
         chisq.val <- (-2*lme4::logLik(m1$value))-(-2*lme4::logLik(m2$value))
@@ -200,57 +256,94 @@ getLmer.chisq <- function(m1,m2) {
 
 #' Fit mixed-models and obtain p-values from model comparison
 #'
-#' A set of model formula are fed to \code{lrCompare} as named
-#' elements in a list.  These models are fit using
-#' \code{\link{tryFit}}.  Models are compared as specified in the
-#' argument \code{lrc.modCompare} using LLR (likelihood-ratio) tests.
-#' This function is useful for performing multiple tests on relatively
-#' complex designs (e.g., all main effects and interactions in a
-#' factorial design).
+#' Compare a set of models to a single 'base' model.  The base model
+#' is fit using \code{\link{tryFit}}, and all subsequent models using
+#' \code{\link{tryUpdate}}.  Models are compared using the
+#' likelihood-ratio test.
+#'
+#' This function is useful for testing multiple IVs in complex designs
+#' (e.g., all main effects and interactions in a factorial design).
 #'
 #' @param mcr.data Data to be analyzed
-#' @param lrc.mods List of (named) model formulae
-#' @param lrc.modCompare List of comparisons to perform (named)
+#' @param lrc.mods List containing model formulae.  The first element
+#' specifies the 'base' model that will be compared to all subsequent
+#' models (specified, in turn, by the remaining elements of the list).
+#' The base model is fit by a call to \code{tryFit}, while the
+#' remaining models are fit by \code{tryUpdate}, and thus should have
+#' formulae as defined in \code{\link{update.formula}}.
+#' @param lrc.modCompare A vector of mode 'character'.  The first
+#' element names the 'base' model listed in \code{lrc.mods}, and the
+#' remaining elements name the models to be compared to 'base'.  If
+#' NULL (default) then automatically compares \code{lrc.mods[1]}
+#' to all subsequent models in \code{lrc.mods}.
+#' @param lrc.method Currently, no effect.  This argument has been
+#' reserved for future development
+#' @param lrc.cluster A processing cluster (typically derived from
+#' \code{\link{makeCluster}} in the \code{parallel} package.  NULL
+#' means run on a single core.
 #' @param ... Arguments to be passed along to \code{\link{tryFit}}
 #' (and thus to \code{lmer}).
 #' @return A vector with the p-values from tests specified by
 #' \code{lrc.modCompare}. If a test could not be performed (e.g.,
 #' because one or both of the corresponding models did not converge),
 #' \code{NA} is returned.
+#' @seealso \code{\link{tryFit}}, \code{\link{tryUpdate}}, \code{\link{update}}, \code{\link{update.formula}}
 #'
 #' @examples
 #' dat <- mkDf()
 #' 
 #' lrCompare(dat,
-#'           lrc.mods=list(
-#'               ri=    Resp~Cond+(1|SubjID)+     (1|ItemID),
-#'               ri.noC= Resp~     (1|SubjID)+     (1|ItemID),
-#'               max=    Resp~Cond+(1+Cond|SubjID)+(1+Cond|ItemID),
-#'               max.noC=Resp~     (1+Cond|SubjID)+(1+Cond|ItemID)),
-#'           lrc.modCompare=list(
-#'               ri=c("ri",  "ri.noC"),
-#'               max=c("max","max.noC")),
+#'           lrc.mods=c(max=Resp~Cond+(1+Cond|SubjID)+(1+Cond|ItemID),
+#'                      max.noC=.~.-Cond,
+#'           lrc.modCompare=c("max","max.noC"),
 #'           REML=FALSE, na.action=na.omit) # pass to tryFit/lmer
 #' 
+#' lrCompare(dat,  # same effect as above, with modCompare set to NULL
+#'           lrc.mods=c(max=    Resp~Cond+(1+Cond|SubjID)+(1+Cond|ItemID),
+#'                      max.noC=Resp~     (1+Cond|SubjID)+(1+Cond|ItemID)),
+#'           lrc.modCompare=NULL, # compare all to first
+#'           REML=FALSE, na.action=na.omit) # pass to tryFit/lmer
+#'
 #' dat <- mkDf.facMixedAB(nitem=24)
 #' 
 #' lrCompare(dat,
 #'           lrc.mods=list(
-#'               m=   Y~Ad*Bd+   (1+Bd|SubjID)+(1+Ad*Bd|ItemID),
-#'               m.A= Y~Bd+Ad:Bd+(1+Bd|SubjID)+(1+Ad*Bd|ItemID),
-#'               m.B= Y~Ad+Ad:Bd+(1+Bd|SubjID)+(1+Ad*Bd|ItemID),
-#'               m.AB=Y~Ad+Bd+   (1+Bd|SubjID)+(1+Ad*Bd|ItemID)),
-#'           lrc.modCompare=list(
-#'               A=c("m","m.A"), B=c("m","m.B"), AB=c("m","m.AB")),
+#'               base=Y~Ad*Bd+(1+Bd|SubjID)+(1+Ad*Bd|ItemID),
+#'               A=.~.-Ad, B=.~.-Bd, AB=.~.-Ad:Bd),
 #'           REML=FALSE, na.action=na.omit)
 #' @export lrCompare
-lrCompare <- function(mcr.data, lrc.mods, lrc.modCompare, ...) {
-    extraArgs <- c(list(tf.data=mcr.data), list(...))
-    argsToTryFit <- c(list(X=lrc.mods, FUN=tryFit), extraArgs)
-    modres <- do.call(lapply, argsToTryFit)
-    unlist(lapply(lrc.modCompare, function(x) {
-        getLmer.pValue(modres[[x[[1]]]], modres[[x[[2]]]])["p"]
+#' @importFrom parallel parLapplyLB
+lrCompare <- function(mcr.data, lrc.mods, lrc.modCompare=NULL, lrc.method="update",
+                      lrc.cluster=NULL, ...) {
+    if (length(lrc.mods) < 2) {
+        stop("'lrc.mods' must contain at least two model formulae")
+    } else {}
+    if (is.null(names(lrc.mods))) { # no names; create them
+        if (!is.null(lrc.modCompare)) {
+            stop("names provided in 'lrc.modCompare', but elements of 'lrc.mods' were not named")
+        } else {}
+        names(lrc.mods) <- c("base", paste("M", 1:(length(lrc.mods)-1), sep=""))
+    } else {}
+    if (is.null(lrc.modCompare)) {
+        lrc.modCompare <- c(names(lrc.mods)[1], names(lrc.mods)[2:length(lrc.mods)])
+    } else {}
+    # error checking...
+    modsNotFit <- setdiff(lrc.modCompare, names(lrc.mods))
+    if (length(modsNotFit)!=0) {
+        stop("model(s) '", paste(lrc.modCompare, collapse=", "), "' not in 'lrc.mods'")
+    } else {}    
+    base.mod <- do.call(tryFit, c(list(tf.formula=lrc.mods[[1]], tf.data=mcr.data), list(...)))
+    if (is.null(lrc.cluster)) {
+        modres <- lapply(lrc.mods[-1], tryUpdate, tu.model=base.mod)
+    } else {
+        initializeCluster(lrc.cluster)
+        modres <- parallel:::parLapplyLB(lrc.cluster, lrc.mods[-1], tryUpdate, tu.model=base.mod)
+    }
+    ff <- do.call("rbind", lapply(lrc.modCompare[-1], function(y) {
+        lrTest(base.mod, modres[[y]])[["Likelihood-Ratio Test"]]
     }))
+    rownames(ff) <- paste(lrc.modCompare[1], lrc.modCompare[-1], sep=" vs ")
+    return(ff)
 }
 
 #' Generate population parameters from data-generating parameters
